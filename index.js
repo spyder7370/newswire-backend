b3f2002814c6320ab79c9cfe7408ec5a9451741a
Hitesh Yadav <53417879+spyder7370@users.noreply.github.com>
2023-05-06 22:01:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+	mongoose = require('mongoose');
+const app = express();
+const cors = require('cors');
+
+mongoose
+	.connect('mongodb+srv://admin:admin@cluster0.25nlq3g.mongodb.net/?retryWrites=true&w=majority')
+	.then(() => console.log('DB Connected!'))
+	.catch((error) => console.log(error));
+
+app.use(express.json());
+app.use(cors());
+const newsRoutes = require('./routes/news');
+app.use(newsRoutes);
+
+app.listen(3001, () => {
+	console.log('server started at port 3001');
+});
